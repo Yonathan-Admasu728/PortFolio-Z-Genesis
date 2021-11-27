5cc38a4a118e37fb014cb799ec29d5f8b71bcfb7
yonathan Admasu <admyoni4@yahoo.com>
2021-11-27 00:22:49 -0800

    fixed the submit button on contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,7 +5,7 @@ import '../stylesheets/contact.css';
 // import AOS from "aos";
 import "aos/dist/aos.css";
 
-import {FaFacebook, FaLinkedin, FaTwitter, FaInstagram, FaGithubSquare, FaFileDownload } from 'react-icons/fa'
+import { FaFileDownload } from 'react-icons/fa'
 
 const Contact = () => {
     
@@ -27,24 +27,16 @@ const Contact = () => {
                         <input type="mail" id="mailid" name="Email" placeholder="Email Address" required></input>
                         <input type="text" id="sub" name="Subject" placeholder="Subject" required></input>
                         <textarea id="msg" name="message" placeholder="Message" required></textarea>
-                        <button style={{cursor: 'pointer'}} type="submit">Send Message</button>
+                        <button style={ {background: '#786399'}} type="submit">Send Message</button>
                     </form>
                 </div>
-                <div className="col-12 col-sm-6 half">
-                    <p className="lead">
-                        {contact.pitch}        
-                        
+                <div className="col-12 col-sm-6 half " >
+                    <p>
+                    Feel free to contact me by submitting this form, clicking on LinkedIn, Tweeter,and please click on Resume icons to see my Resume.
                     </p>
                     <div className="rs">
                 <div className="d-flex justify-content-center align-items-center flex-column ">
-                <div className="d-flex justify-content-center align-items-center flex-row">
-                    {social.linkedin && <a title="Visit Linkedin profile" rel="noopener noreferrer" target="_blank"  href={social.linkedin}><FaLinkedin color='darkblue' fontSize='2em'/></a>}
-                    {social.facebook && <a title="Visit Facebok profile" rel="noopener noreferrer" target="_blank" href={social.facebook}><FaFacebook color='blue' fontSize='2em'/></a>}
-                    {social.twitter && <a title="Visit Twitter profile" rel="noopener noreferrer" target="_blank" href={social.twitter}><FaTwitter color='aqua' fontSize='2em'/></a>}
-                    {social.instagram && <a title="Visit Instagram profile" rel="noopener noreferrer" target="_blank" href={social.instagram}><FaInstagram color='orange' fontSize='2em'/></a>}
-                    {social.github && <a title="Visit Github profile" rel="noopener noreferrer" target="_blank" href={social.github}><FaGithubSquare color='white' fontSize='2em'/></a>}<br/>
-                </div>
-                    {social.resume && <a title="Download Resume" href={social.resume} download><FaFileDownload color='green' fontSize='2em'/></a>}
+                    {social.resume && <a title="Download Resume" href={social.resume} download><span className='res-download'>resume</span><br/><FaFileDownload color='#6207f0' fontSize='3em' opacity='0.5'/></a>}
                 </div>
                 </div>
                 </div>
